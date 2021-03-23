e0f2aa9d1721fda7936f59479eca0d7fd60236e4
brundewald <nikyiro@gmail.com>
2021-03-23 12:18:53 +0300

    Добавьте файлы проекта.

---
+++ Fractions_Library/Fractions.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Fractions_Library
+{
+    public struct Fraction
+    {
+        public int i, n, d; //integer, numerator, divisor
+        
+        public Fraction(int i, int n, int d) 
+        {
+            this.i = i;
+            this.n = n;
+            this.d = d;
+        }
+
+        public static Fraction Simplify(int i, int n, int d)
+        {
+            
+            if (n % 2 == 0 && d % 2 == 0) // это условие проверяет кратность и отдаёт сокращённую дробь
+            {
+                while (n > d)
+                {
+                    i = n / d;
+                    n = n - i * d;
+                    
+                }
+
+                while (n % 2 == 0 && d % 2 == 0)
+                {
+                    n = n / 2;
+                    d = d / 2;
+                }
+                                              
+
+                return new Fraction(
+                i: i,
+                n: n,
+                d: d
+                );
+
+                
+            }
+
+            if (n >= d)
+            {
+                while (n >= d) // это условие получает целое число если знаменатель больше делителя
+                {
+                    i = n / d;
+                    n = n - i * d; 
+                    
+                }
+                return new Fraction(
+                i: i,
+                n: n,
+                d: d
+                );
+            }
+
+            else return new Fraction(
+                i: i,
+                n: n,
+                d: d
+                );      // в случае если дробь правильная и не подлежит упрощению, то выводит результат
+        }
+
+        //public override Fraction operator +() 
+        //{
+            
+        //}
+
+
+    
+        //public static Fraction integer_calculation (int i, int n, int d)
+        //{
+                       
+        //    return new Fraction(
+        //        i: i + (n/d),
+        //        n: ,
+        //        d:
+        //        );
+               
+          
+        //}
+
+        public override string ToString()
+        {
+            if (n > 0 && n < d && i > 0) return $"{i},{n}/{d}";
+            else if (i > 0 && n == 0) return $"{i}";
+            else return $"{n}/{d}";
+        }
+    }
+}
---
+++ Lesson_3_Fractions/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Fractions_Library;
+
+namespace Lesson_3_Fractions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi! This program will help you with rational fractions.\n");
+            while (true)
+            {
+                int a;
+                
+                
+
+                Fraction nmbr = new Fraction(0, 0, 0);
+                
+                a = getNumerator(ref nmbr);
+                a = getDivisor(ref nmbr);
+
+                nmbr = Fraction.Simplify(nmbr.i, nmbr.n, nmbr.d);
+
+                Console.Write($"You entered fraction: {nmbr}\n");
+                                
+                Console.Write("Do you want to repeat? (y/n): ");
+                string rpt = Console.ReadLine();
+
+                if (rpt == "n") break;
+            }
+            Console.Write("Bye!");
+        }
+
+        private static int getDivisor(ref Fraction nmbr)
+        {
+            int a;
+            while (true)
+            {
+
+                Console.Write("\nEnter the divisor:");
+                var d = int.TryParse(Console.ReadLine(), out a);
+                if (a > 0 && d)
+                {
+                    nmbr.d = a;
+                    break;
+                }
+
+            }
+
+            return a;
+        }
+
+        private static int getNumerator(ref Fraction nmbr)
+        {
+            int a;
+            while (true)
+            {
+                Console.Write("\nEnter the numerator:");
+                var n = int.TryParse(Console.ReadLine(), out a);
+
+                if (a > 0 && n)
+                {
+                    nmbr.n = a;
+                    break;
+                }
+
+            }
+
+            return a;
+        }
+    }
+}
