222198be40cd70bfe0b6dc0276a8285e4a4ddced
brundewald <nikyiro@gmail.com>
2021-03-23 14:53:43 +0300

    Отладил вычисление дробей и добавил функционал математических операций.

--- Lesson_3_Fractions/Program.cs
+++ Lesson_3_Fractions/Program.cs
@@ -16,29 +16,90 @@ namespace Lesson_3_Fractions
         {
             while (true)
             {
-                int a;
 
+                int a = 0;
+                int b = 0;
+                Fraction nmbr1 = new Fraction(0, 0, 0);
+                Fraction nmbr2 = new Fraction(0, 0, 0);
 
+                Console.WriteLine("Enter first fraction");
+                
+                nmbr1.n = getNumerator(ref b);
+                nmbr1.d = getDivisor(ref a);
+                Console.Write($"First fraction: {nmbr1}\n");
 
-                Fraction nmbr = new Fraction(0, 0, 0);
+                Console.WriteLine("Enter second fraction");
+                nmbr2.n = getNumerator(ref b);
+                nmbr2.d = getDivisor(ref a);
+                Console.Write($"Second fraction: {nmbr2}\n");
+                
+                Fraction sum = nmbr1 + nmbr2;   //sum
+                Console.Write($"\nHere is sum: {sum}\n");
 
-                a = getNumerator(ref nmbr);
-                a = getDivisor(ref nmbr);
+                Fraction sumS = Fraction.Simplify(sum.i, sum.n, sum.d);
+                Console.Write($"\nHere is simplicated sum: {sumS}\n");
 
-                nmbr = Fraction.Simplify(nmbr.i, nmbr.n, nmbr.d);
+                Fraction sub = nmbr1 - nmbr2;   //substraction
+                Console.Write($"\nHere is substraction: {sub}\n");
 
-                Console.Write($"You entered fraction: {nmbr}\n");
+                Fraction subS = Fraction.Simplify(sub.i, sub.n, sub.d);
+                Console.Write($"\nHere is simplicated substraction: {subS}\n");
 
-                Console.Write("Do you want to repeat? (y/n): ");
+                Fraction mul = nmbr1 * nmbr2; //multiplication
+                Console.Write($"\nHere is multiplication: {mul}\n");
+
+                Fraction mulS = Fraction.Simplify(mul.i, mul.n, mul.d);
+                Console.Write($"\nHere is simplicated multiplication: {mulS}\n");
+
+                Fraction div = nmbr1 / nmbr2; //divide
+                Console.Write($"\nHere is divide: {div}\n");
+
+                Fraction divS = Fraction.Simplify(div.i, div.n, div.d);
+                Console.Write($"\nHere is simplicated divide: {divS}\n");
+
+                double decSum = sum.n / sum.d;
+                Console.Write($"\nHere is sum decimal: {decSum:F2}\n");
+
+                double decSub = sub.n / sub.d;
+                Console.Write($"\nHere is sum decimal: {decSub:F2}\n");
+
+                double decMul = mul.n / mul.d;
+                Console.Write($"\nHere is sum decimal: {decMul:F2}\n");
+
+                double decDiv = div.n / div.d;
+                Console.Write($"\nHere is sum decimal: {decDiv:F2}\n");
+
+                Console.Write("If you don't want to repeat press 'n': ");
                 string rpt = Console.ReadLine();
 
                 if (rpt == "n") break;
             }
+           
         }
 
-        private static int getDivisor(ref Fraction nmbr)
+      
+
+        private static int getNumerator(ref int b)
         {
-            int a;
+            
+            while (true)
+            {
+                Console.Write("\nEnter the numerator:");
+                var n = int.TryParse(Console.ReadLine(), out b);
+
+                if (b > 0 && n)
+                {
+                    break;
+                }
+
+            }
+
+            return b;
+            }
+         
+        private static int getDivisor(ref int a)
+        {
+            
             while (true)
             {
 
@@ -46,7 +107,7 @@ namespace Lesson_3_Fractions
                 var d = int.TryParse(Console.ReadLine(), out a);
                 if (a > 0 && d)
                 {
-                    nmbr.d = a;
+                    
                     break;
                 }
 
@@ -54,24 +115,5 @@ namespace Lesson_3_Fractions
 
             return a;
         }
-
-        private static int getNumerator(ref Fraction nmbr)
-        {
-            int a;
-            while (true)
-            {
-                Console.Write("\nEnter the numerator:");
-                var n = int.TryParse(Console.ReadLine(), out a);
-
-                if (a > 0 && n)
-                {
-                    nmbr.n = a;
-                    break;
-                }
-
-            }
-
-            return a;
-        }
-    }
+    }   
 }
