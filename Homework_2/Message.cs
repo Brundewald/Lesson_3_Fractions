857e03a37273fb10dd7ffa74912063fa3f871141
brundewald <nikyiro@gmail.com>
2021-03-31 16:52:38 +0300

    Попытка осуществить фильтрацию через StringBuilder и массивы.

--- Homework_2/Message.cs
+++ Homework_2/Message.cs
@@ -6,20 +6,55 @@ namespace Homework_2
 {
     class Message
     {
-        public static void Start() 
+        public static void Start()
+        {
+            string msg;
+            int nmbr;
+            StringBuilder nsb = new StringBuilder();
+            char ch;
+            int count = 0;
+            string[] word = new string [count];
+            string cw;            
+            getData(out msg, out nmbr);
+            for (int i = 0; i <= msg.Length - 1; i++)
+            {
+                ch = msg[i];
+                if (char.IsLetter(ch) == true)
+                {
+                    nsb = nsb.Append(ch);
+                    Console.WriteLine(ch);
+                    Console.WriteLine(nsb);
+                }
+                else
+                {
+                    if (nsb.Length == nmbr)
+                    {
+                        count++;
+                        cw = nsb.ToString();
+                        word[count-1] = cw;
+                        nsb.Clear();
+                    }
+                    else nsb.Clear();
+                }
+            }
+            if (nsb.Length == nmbr)
+            {
+                count++;
+                cw = nsb.ToString();
+                word[count-1] = cw;
+                nsb.Clear();
+            }
+            Console.Write(word.ToString());
+        }
+
+        private static void getData(out string msg, out int nmbr)
         {
             Console.WriteLine("This program can read your message.\nFirst enter your message, and after enter a number.\n");
-            string msg = Console.ReadLine();
+            msg = Console.ReadLine();
             Console.Write($"\nMessage: {msg}\n");
-            int nmbr = Convert.ToInt32(Console.ReadLine());
-            Console.Write($"\nNumber: {nmbr}\n");
-            string word;
 
-            if (char.IsLetter(msg))
-            {
-                Console.WriteLine(msg);
-            }
-            else Console.WriteLine("There is no word wich contains this amount of chars.");
+            nmbr = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"\nNumber: {nmbr}\n");
         }
     }
 }
