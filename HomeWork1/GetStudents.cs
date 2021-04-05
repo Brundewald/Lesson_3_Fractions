d337ab0cd9f5a7cc442ce59de16e30046c66b00b
brundewald <nikyiro@gmail.com>
2021-04-05 16:29:53 +0300

    Наприсал часть с вызовом потока чтения из файла и сбором информации.

--- HomeWork1/GetStudents.cs
+++ HomeWork1/GetStudents.cs
@@ -4,16 +4,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Collections;
 
 
 namespace HomeWork3
 {
     class GetStudents
     {
-        int bach = 0;
-        int mast = 0;
+        public static void StudentsData() 
+        {
+            int bach = 0;
+            int mast = 0;
 
-        StreamReader sr = new StreamReader("/students.csv");
+            ArrayList list = new ArrayList();
+           
+
+            StreamReader sr = new StreamReader("C:\\Users\\nsm\\source\\repos\\Lesson_3_Fractions\\students.csv");
+            while (!sr.EndOfStream)
+            {
+                try
+                {
+                    string[] s = sr.ReadLine().Split(';');
+                    list.Add(s[1] + " " + s[0]);
+                    if (int.Parse(s[6]) < 6) bach++;
+                    else mast++;
+
+                }
+                catch { }
+            }
+            sr.Close();
+            list.Sort();
+            foreach (var v in list) 
+            {
+                Console.WriteLine(v);
+            }
+        }
+        
 
     }
 }
