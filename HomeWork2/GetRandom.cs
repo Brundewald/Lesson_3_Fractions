87b4bbd10b2a53c60064356dbb7aaceca2657f34
brundewald <nikyiro@gmail.com>
2021-04-12 14:00:56 +0300

    Приступил к 4й задаче ДЗ к 6 вебинару.

---
+++ HomeWork2/GetRandom.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace HomeWork4
+{
+    class GetRandom
+    {
+        public static void RandomFile() 
+        {
+            Random i = new Random();
+            string[] rand = new string[10];
+            StreamWriter sw = new StreamWriter("random_numbers.txt");
+            for (int v = 0; v <= 10 - 1; v++) 
+            {                 
+                sw.Write($"{rand[v] = Convert.ToString(i.Next(0, 100))} ");
+                sw.Flush();
+            }
+            sw.Close();
+        }
+        //public static void ReadFileSR() 
+        //{
+        //    StreamReader sr = new StreamReader("random_numbers.txt");
+        //    while (!sr.EndOfStream)
+        //    {
+        //        string[] s = 
+        //    }
+        //}
+    }
+}
--- HomeWork2/Program.cs
+++ HomeWork2/Program.cs
@@ -2,13 +2,13 @@
 
 /*Created by Nikita Mialkin for GeekBrains*/
 
-namespace HomeWork2
+namespace HomeWork4
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DataHandler.Run();
         }
     }
 }
